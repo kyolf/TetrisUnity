40853a0c828d4a8cf15d7cd405202906a8174faf
Kyle Szeto <kyl3sz3t0@gmail.com>
2019-03-12 20:12:46 -0700

    left right down arrow

--- Assets/Scripts/Tetramino.cs
+++ Assets/Scripts/Tetramino.cs
@@ -13,6 +13,30 @@ public class Tetramino : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        CheckUserInput();
+    }
+
+    void CheckUserInput()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, -1, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+        }
     }
 }
